bb1fd10bfab857c3ab21488873e5d03f6c1d1521
abw2007 <andrewworth41@gmail.com>
2023-02-27 14:25:51 -0700

    hh config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    // goerli: {
+    //   url: GOERLI_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 5,
+    // },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet (network 1) it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
   solidity: "0.8.17",
 };
