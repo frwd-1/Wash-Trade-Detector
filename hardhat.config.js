28fa24c97be632e586a17cbcd963e03938aa0a46
abw2007 <andrewworth41@gmail.com>
2023-03-04 19:28:56 -0700

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,14 +17,14 @@ module.exports = {
       chainId: 5,
     },
     localhost: {
-      url: "http://127.0.0.1:8545/",
+      url: "",
       chainId: 31337,
     },
   },
   namedAccounts: {
     deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet (network 1) it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+      default: 0, 
+      1: 0, 
     },
   },
   solidity: "0.8.17",
