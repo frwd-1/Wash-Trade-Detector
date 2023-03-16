fe7fc711519555c97285007a59a31d43412835b1
abw2007 <andrewworth41@gmail.com>
2023-03-15 19:39:38 -0600

    expands scope

--- src/agent.ts
+++ src/agent.ts
@@ -10,6 +10,7 @@ import {
 import { getBuyer, getSeller, getNftId } from "./utils";
 import {
   EXCHANGE_CONTRACT_ADDRESSES,
+  // EXCHANGE_CONTRACT_ADDRESSES,
   EXCHANGE_TRADE_EVENTS,
   TRANSFER_EVENT,
 } from "./constants";
@@ -19,20 +20,30 @@ const provider = new ethers.providers.EtherscanProvider(
   process.env.ETHERSCAN_API_KEY
 );
 
-const loadConfig = () => {
-  try {
-    return require("./bot-config.json");
-  } catch (e) {
-    return require("../bot-config.json");
-  }
-};
+// const loadConfig = () => {
+//   try {
+//     return require("./bot-config.json");
+//   } catch (e) {
+//     return require("../bot-config.json");
+//   }
+// };
 
-const config = loadConfig();
+// const config = loadConfig();
 
-const { nftCollectionAddress, nftCollectionName, nftExchangeName } = config;
+// const { nftCollectionAddress, nftCollectionName, nftExchangeName } = config;
+const nftCollectionAddress = "test";
+const nftCollectionName = "test";
+const nftExchangeName = "test";
 
-const tradeEvent: string = EXCHANGE_TRADE_EVENTS[nftExchangeName];
-const nftExchangeAddress: string = EXCHANGE_CONTRACT_ADDRESSES[nftExchangeName];
+// const exchangeAddresses = Object.values(EXCHANGE_CONTRACT_ADDRESSES);
+const exchangeTrades = Object.values(EXCHANGE_TRADE_EVENTS);
+// const exchangeAddresses = Object.values(EXCHANGE_CONTRACT_ADDRESSES);
+// const exchangeAddressFilters = Object.values(EXCHANGE_CONTRACT_ADDRESSES).map(
+//   (address) => ({
+//     address: address.toLowerCase(),
+//   })
+// );
+
 let numberOfTrades: number = 0;
 let numberOfWashTrades: number = 0;
 
@@ -128,7 +139,7 @@ async function checkRelationship(transfer: LogDescription): Promise<Finding[]> {
         token: `Wash Traded NFT Token ID: ${tokenId}`,
         collectionContract: nftCollectionAddress,
         collectionName: nftCollectionName,
-        exchangeContract: nftExchangeAddress,
+        // exchangeContract: "test", //exchangeAddresses,
         exchangeName: nftExchangeName,
         anomalyScore: `${
           (numberOfWashTrades / numberOfTrades) * 100
@@ -147,15 +158,45 @@ async function checkRelationship(transfer: LogDescription): Promise<Finding[]> {
 const handleTransaction: HandleTransaction = async (txEvent) => {
   const findings: Finding[] = [];
 
-  const tradeEvents = txEvent.filterLog(tradeEvent, nftExchangeAddress);
-
-  const transferEvents = txEvent.filterLog(
-    TRANSFER_EVENT,
-    nftCollectionAddress
+  const tradeEvents = txEvent.filterLog(exchangeTrades);
+  // const exchangeAddressEvents = txEvent.filterLog(exchangeAddressFilters);
+  const exchangeAddressEvents: LogDescription[] = tradeEvents.filter(
+    (log: LogDescription) =>
+      Object.values(EXCHANGE_CONTRACT_ADDRESSES).some(
+        (address) => log.address.toLowerCase() === address.toLowerCase()
+      )
   );
 
-  // checks that the transfers are for the trades
-  if (tradeEvents.length == transferEvents.length) {
+  const transferEvents = txEvent.filterLog(TRANSFER_EVENT);
+
+  // log results
+  if (tradeEvents.length > 0 && exchangeAddressEvents.length > 0) {
+    console.log(tradeEvents);
+    console.log(`tradeEvent length is ${tradeEvents.length}`);
+    console.log(
+      `exchangeAddressEvent length is ${exchangeAddressEvents.length}`
+    );
+  }
+  // log results
+  if (
+    tradeEvents.length > 0 &&
+    exchangeAddressEvents.length > 0 &&
+    transferEvents.length > 0
+  ) {
+    console.log(`tradeEvent length is ${tradeEvents.length}`);
+    console.log(
+      `exchangeAddressEvent length is ${exchangeAddressEvents.length}`
+    );
+    console.log(`transferEvent length is ${transferEvents.length}`);
+  }
+
+  // checks that the transfers are for the trades on a monitored NFT exchange
+  if (
+    tradeEvents.length > 0 &&
+    transferEvents.length > 0 &&
+    transferEvents.length < 4 &&
+    exchangeAddressEvents.length > 0
+  ) {
     for (let i = 0; i < transferEvents.length; i++) {
       const transfer = transferEvents[i];
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,25 @@
 export const EXCHANGE_CONTRACT_ADDRESSES: Record<string, string> = {
   LooksRare: "0x59728544B08AB483533076417FbBB2fD0B17CE3a", // LooksRare: Exchange
   OpenSea: "0x7f268357A8c2552623316e2562D90e642bB538E5", // OpenSea: Wyvern Exchange v2
-  NftMarketplace: "0x3819579b236e5Ab5C695DD4762c2B18bB0Aee1c8",
+  Rarible: "0x9757F2d2b135150BBeb65308D4a91804107cd8D6", // Rarible: Exchange V2
+  SuperRare: "0x6D7c44773C52D396F43c2D511B81aa168E9a7a42",
+  X2Y2: "0x74312363e45DCaBA76c59ec49a7Aa8A65a67EeD3",
+  NftMarketplace: "0x3819579b236e5Ab5C695DD4762c2B18bB0Aee1c8", // test (goerli)
 };
+
 export const EXCHANGE_TRADE_EVENTS: Record<string, string> = {
   LooksRare:
     "event TakerBid(bytes32 orderHash, uint256 orderNonce, address indexed taker, address indexed maker, address indexed strategy, address currency, address collection, uint256 tokenId, uint256 amount, uint256 price)",
   OpenSea:
     "event OrdersMatched(bytes32 buyHash, bytes32 sellHash, address indexed maker, address indexed taker, uint price, bytes32 indexed metadata)",
+  Rarible:
+    "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+  SuperRare:
+    "event AuctionSettled(address indexed _contractAddress, address indexed _bidder, address _seller, uint256 indexed _tokenId, address _currencyAddress, uint256 _amount)",
+  X2Y2: "event EvProfit(bytes32 itemHash, address currency, address to, uint256 amount)",
   NftMarketplace:
     "event ItemBought(address indexed buyer, address indexed seller, address indexed nftAddress, uint256 tokenId, uint256 price)",
 };
-
-// adjust parameters to target desired time frame
-export const MAX_AGE = 24 * 60 * 60 * 30;
-export const MAX_DEPTH = 50;
-export const MAX_SALES = 1;
-export const MAX_TOKENS = 100000;
 
 export const TRANSFER_EVENT =
   "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)";
