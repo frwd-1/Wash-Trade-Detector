00e86f1809ea643dee5ddffc343279743c8502b9
abw2007 <andrewworth41@gmail.com>
2023-08-25 11:21:16 -0600

    Add database tables

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,16 +2,25 @@ import { Database } from "sqlite3";
 
 const sqlite3 = require("sqlite3").verbose();
 
-const db: Database = new sqlite3.Database("./src/database/transactions.db");
+const db: Database = new sqlite3.Database("./src/database/sybil_detection.db");
 
 db.serialize(() => {
+  // Transactions Table
   db.run(
     "CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, buyer TEXT, seller TEXT, transactionDate TEXT, nftContractAddress TEXT, nftId TEXT, exchangeAddress TEXT)",
-    (err: Error | null) => {
-      if (err) {
-        console.error("Error creating table:", err);
-      }
-    }
+    handleError
+  );
+
+  // Sybil Addresses Table
+  db.run(
+    "CREATE TABLE IF NOT EXISTS sybil_addresses (id INTEGER PRIMARY KEY, address TEXT UNIQUE, firstDetectedDate TEXT)",
+    handleError
+  );
+
+  // Sybil Clusters Table
+  db.run(
+    "CREATE TABLE IF NOT EXISTS sybil_clusters (id INTEGER PRIMARY KEY, clusterId INTEGER, address TEXT, addedDate TEXT)",
+    handleError
   );
 });
 
@@ -37,18 +46,60 @@ export async function addToDatabase(
       exchangeAddress,
       (err: Error | null) => {
         if (err) {
-          console.error("Error inserting into table:", err);
+          console.error("Error inserting into transactions table:", err);
           reject(err);
           return;
         }
-        stmt.finalize((err: Error | null) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve();
-        });
+        stmt.finalize(handleError);
+        resolve();
       }
     );
   });
+}
+
+export async function addSybilAddress(
+  address: string,
+  detectedDate: string
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const stmt = db.prepare(
+      "INSERT OR IGNORE INTO sybil_addresses (address, firstDetectedDate) VALUES (?, ?)"
+    );
+    stmt.run(address, detectedDate, (err: Error | null) => {
+      if (err) {
+        console.error("Error inserting into sybil_addresses table:", err);
+        reject(err);
+        return;
+      }
+      stmt.finalize(handleError);
+      resolve();
+    });
+  });
+}
+
+export async function addAddressToCluster(
+  clusterId: number,
+  address: string,
+  addedDate: string
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    const stmt = db.prepare(
+      "INSERT INTO sybil_clusters (clusterId, address, addedDate) VALUES (?, ?, ?)"
+    );
+    stmt.run(clusterId, address, addedDate, (err: Error | null) => {
+      if (err) {
+        console.error("Error inserting into sybil_clusters table:", err);
+        reject(err);
+        return;
+      }
+      stmt.finalize(handleError);
+      resolve();
+    });
+  });
+}
+
+function handleError(err: Error | null): void {
+  if (err) {
+    console.error("Database error:", err);
+  }
 }
