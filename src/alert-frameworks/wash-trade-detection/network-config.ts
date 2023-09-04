ccfb3786410e49e2367949ba4be185c084eb1e79
abw2007 <andrewworth41@gmail.com>
2023-09-04 14:19:42 -0600

    Update readme

---
+++ src/alert-frameworks/wash-trade-detection/network-config.ts
@@ -0,0 +1,30 @@
+import {
+  ethProvider,
+  avaxProvider,
+  ftmProvider,
+  optProvider,
+  mtcProvider,
+  bscProvider,
+  arbProvider,
+} from "../wash-trade-detection/constants";
+
+export function getProviderForNetwork(chainId: number) {
+  switch (chainId) {
+    case 1:
+      return ethProvider;
+    case 43114:
+      return avaxProvider;
+    case 250:
+      return ftmProvider;
+    case 10:
+      return optProvider;
+    case 137:
+      return mtcProvider;
+    case 56:
+      return bscProvider;
+    case 42161:
+      return arbProvider;
+    default:
+      throw new Error(`Unsupported network ID: ${chainId}`);
+  }
+}
